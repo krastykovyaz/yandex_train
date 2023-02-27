72ad586b5068447ea12cc144c092d1a3517b0b4b
krastykovyaz <krasty_90@mail.ru>
2023-02-27 18:37:59 +0300

    add alchogolic

--- diego_collector.py
+++ diego_collector.py
@@ -1,30 +1,44 @@
 from typing import List
 def get_sticker(diego: List[int], collectors: List[int])->None:
-    def is_in(diego_l: List[int], target: int)->int:
-        save = []
-        count = 0
-        i = 0
-        while i < len(diego_l) and diego_l[i] < target:
-            if diego_l[i] not in save:
-                save.append(diego_l[i])
-                count += 1
-            i += 1
-        return count
-    for col in collectors:
-        print(is_in(diego, col))
+    res = []
+    if len(diego) == 1:
+        for coll in collectors:
+            if coll > diego[0]:
+                print(1)
+            else:
+                print(0)
+    else:
+        for coll in collectors:
+            sorted_lst_orig = sorted(diego)
+            dig = set()
+            diego = [d for d in diego if d not in dig and not dig.add(d)]
+            def looking(sorted_lst: List[int], item: int, idx: int) -> List[int]:
+                if sorted_lst and sorted_lst[-1] < item:
+                    return idx
+                elif len(sorted_lst) == 0:
+                    return 0
+                mid = len(sorted_lst) // 2
+                if item < sorted_lst[mid]:
+                    return looking(sorted_lst[:mid], item, idx - mid)
+                else:
+                    return looking(sorted_lst[mid:], item, idx + mid - 1)
+            print(looking(sorted_lst_orig, coll, len(sorted_lst_orig)))
+
+
+
 
 
 if __name__=='__main__':
-    inpt = '''1
-5
-2
-4 6
+    inpt = '''3
+100 1 50
+3
+300 0 75
 '''
 
     inpt = inpt.split('\n')
-    # diego = int(inpt[0])
-    # inpt = [input() for _ in range(4)]
+    diego = int(inpt[0])
+    inpt = [input() for _ in range(4)]
     diego_list = list(map(int, inpt[1].split()))
-    # collectors = int(inpt[2])
+    collectors = int(inpt[2])
     collectors_list = list(map(int, inpt[3].split()))
     get_sticker(sorted(diego_list),collectors_list)
--- pretty_row.py
+++ pretty_row.py
@@ -1,37 +1,27 @@
 from typing import List
+import sys
 
 
 def get_max(n: int, a: List[str]) -> int:
-    def count_ch(chars: List[str], add: int) -> int:
-        res = 0
-        for i in range(1, len(chars)):
-            count = 1
-            j = i
-            while j < len(chars) and chars[j - 1] == chars[j]:
+    count_list = []
+    for l in range(len(a)):
+        r = l
+        count = 0
+        k = n
+        while r < len(a):
+            if a[r] == a[l]:
                 count += 1
-                j += 1
-            if res < count:
-                res = count
-        remain = len(chars) - res
-        return res + min(add, remain)
-
-    max_count = 0
-    values = set(a)
-    for value in sorted(values):
-        i = 0
-        while i < len(a) - n:
-            tmp_a = list(a)
-            slice = list(value * n)
-            len_char = len([ch for ch in tmp_a[i:i + n] if value == ch])
-            tmp_a[i:i + n] = slice
-            max_count_new = count_ch(tmp_a, len_char)
-            if max_count < max_count_new:
-                max_count = max_count_new
-            i += 1
-    return max_count
+            elif k:
+                count += 1
+                k -= 1
+            else:
+                break
+            r += 1
+        count_list.append(count)
+    return max(count_list)
 
 
 if __name__ == '__main__':
-    n = 2
-    string = "helto"
-    print(get_max(n, string))
+    n = int(sys.stdin.readline())
+    string = sys.stdin.readline()
+    sys.stdout.write(str(get_max(n, string)))
